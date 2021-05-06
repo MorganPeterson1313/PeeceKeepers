e2ae627bdc9b528ce9ef9049615b2680038b97d9
Morgan Peterson <morenalatina@Morgan-MBP.home>
2021-05-06 13:04:48 -0400

    added fluid property to video image

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 import Christmas from "../Assets/Christmas.jpeg";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/esm/Container";
+
 export class Video extends Component {
   render() {
     return (
-      <div style={{ width: 660, height: "auto" }}>
-        <a href="https://www.youtube.com/watch?v=P8qivvdNqUw">
-          <Image src={Christmas} fluid />
-        </a>
+      <div>
+        <Container fluid>
+          <a href="https://www.youtube.com/watch?v=P8qivvdNqUw">
+            <Image
+              src={Christmas}
+              width="auto"
+              height="auto"
+              className="d-block"
+              fluid
+            />
+          </a>
+        </Container>
       </div>
     );
   }
