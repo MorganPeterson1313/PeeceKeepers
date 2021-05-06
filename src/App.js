8a46a1176521cd662e2e781810ca49b82928a70b
Morgan Peterson <morenalatina@Morgan-MBP.home>
2021-05-06 09:04:04 -0400

    added a donation button from paypal and containers to the app

--- src/App.js
+++ src/App.js
@@ -9,84 +9,89 @@ import Events from "./Components/Events";
 import Home from "./Components/Home";
 import Image from "react-bootstrap/Image";
 import { Switch, Route, Link, NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+
 function App() {
   return (
     <div className="App">
-      <Nav.Link href="/">
-        <Image
-          alt="logo"
-          src={peecelogo}
-          width="180px"
-          height="150px"
-          className="d-inline-block align-top"
-          roundedCircle
-          style={{ marginTop: "6%" }}
-          href="/"
-        />
-      </Nav.Link>
-      <Nav
-        className="justify-content-center"
-        activeKey="/"
-        style={{
-          borderTop: "solid 2px DodgerBlue",
-          marginRight: "5%",
-          marginLeft: "5%",
-        }}
-      >
-        <Nav.Item>
-          <Nav.Link style={{ color: "#85200c" }} href="/">
-            Home
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link style={{ color: "#85200c" }} href="/about">
-            About
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link style={{ color: "#85200c" }} href="/donate">
-            {" "}
-            Donate
-          </Nav.Link>
-        </Nav.Item>
+      <Container>
+        <Nav.Link href="/">
+          <Image
+            alt="logo"
+            src={peecelogo}
+            width="180px"
+            height="150px"
+            className="d-inline-block align-top"
+            roundedCircle
+            style={{ marginTop: "6%" }}
+            href="/"
+          />
+        </Nav.Link>
+        <Nav
+          className="justify-content-center"
+          activeKey="/"
+          style={{
+            borderTop: "solid 2px DodgerBlue",
+            marginRight: "5%",
+            marginLeft: "5%",
+          }}
+        >
+          <Nav.Item>
+            <Nav.Link style={{ color: "#85200c" }} href="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link style={{ color: "#85200c" }} href="/about">
+              About
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link style={{ color: "#85200c" }} href="/donate">
+              {" "}
+              Donate
+            </Nav.Link>
+          </Nav.Item>
 
-        {/* <Nav.Item>
+          {/* <Nav.Item>
       <Nav.Link style={{color:"#803CAE"}} href="/services">Services</Nav.Link>
     </Nav.Item> */}
 
-        {/* <p className="text-center mt-4 mb-4">Or right-aligned</p> */}
-        {/* <Nav className="justify-content-end" activeKey="/home"> */}
-        <Nav.Item>
-          <Nav.Link style={{ color: "#85200c" }} href="/events">
-            Events
-          </Nav.Link>
-        </Nav.Item>
-        {/* <Nav.Item>
+          {/* <p className="text-center mt-4 mb-4">Or right-aligned</p> */}
+          {/* <Nav className="justify-content-end" activeKey="/home"> */}
+          <Nav.Item>
+            <Nav.Link style={{ color: "#85200c" }} href="/events">
+              Events
+            </Nav.Link>
+          </Nav.Item>
+          {/* <Nav.Item>
       <Nav.Link style={{color:"#803CAE"}} href="https://www.amazon.com/s?rh=n%3A7141123011%2Cp_4%3AFit+for+the+Soul&ref=bl_sl_s_ap_web_7141123011">Products</Nav.Link>
     </Nav.Item> */}
 
-        <Nav.Item>
-          <Nav.Link style={{ color: "#85200c" }} href="/contact">
-            Contact
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+          <Nav.Item>
+            <Nav.Link style={{ color: "#85200c" }} href="/contact">
+              Contact
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/donate" component={Donate} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/donate" component={Donate} />
 
-        <Route path="/events" component={Events} />
+          <Route path="/events" component={Events} />
 
-        <Route path="/contact" component={Contact} />
-        {/* <Route render={function () {
+          <Route path="/contact" component={Contact} />
+          {/* <Route render={function () {
                             return <p>Not found</p>
                         }} /> */}
-      </Switch>
+        </Switch>
 
-      <footer style={{ textAlign: "center", color: "white", marginTop: "5%" }}>
-        {/* <div style={{display:'flex', justifyContent: 'center'}}>
+        <footer
+          style={{ textAlign: "center", color: "white", marginTop: "5%" }}
+        >
+          {/* <div style={{display:'flex', justifyContent: 'center'}}>
                    
                      <Nav.Link href ="">
                    <Image
@@ -109,9 +114,9 @@ function App() {
                   />{' '}
                   </Nav.Link>
                    </div> */}
-      </footer>
+        </footer>
 
-      {/* <Form style={{margin:"2%", paddingLeft:"20%", paddingRight:"20%"}}>
+        {/* <Form style={{margin:"2%", paddingLeft:"20%", paddingRight:"20%"}}>
 <h4 style={{color:"teal"}}>Subscribe Form</h4>
   <Form.Group controlId="formBasicEmail">
     <Form.Label style={{color:"teal"}}>Email address</Form.Label>
@@ -127,11 +132,12 @@ function App() {
   </Button>
 </Form> */}
 
-      <div style={{ marginBottom: "5%" }}>
-        <h6 style={{ color: "dodgerBlue", fontSize: "15px" }}>
-          © 2021 by Morgan Dalia Peterson
-        </h6>
-      </div>
+        <div style={{ marginBottom: "5%" }}>
+          <h6 style={{ color: "dodgerBlue", fontSize: "15px" }}>
+            © 2021 by Morgan Dalia Peterson
+          </h6>
+        </div>
+      </Container>
     </div>
   );
 }
--- src/Components/Donate.js
+++ src/Components/Donate.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 export class Donate extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Jumbotron style={{ margin: "10%", backgroundColor: "#073763" }}>
+          <h1 style={{ color: "white " }}>Donate</h1>
+          <p style={{ color: "white " }}>
+            We are raising money for Community Outreach Initiatives. I've raised
+            $100 so far, but I still have a ways to go to completely reach my
+            goal! Would you consider making a donation (even small gifts help!)
+            to assist me in my fundraising efforts?
+          </p>
+          <p>
+            <form
+              action="https://www.paypal.com/donate"
+              method="post"
+              target="_top"
+            >
+              <input
+                type="hidden"
+                name="hosted_button_id"
+                value="2AK3T92WAYZ68"
+              />
+              <input
+                type="image"
+                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                border="0"
+                name="submit"
+                title="PayPal - The safer, easier way to pay online!"
+                alt="Donate with PayPal button"
+              />
+              <img
+                alt=""
+                border="0"
+                src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+                width="1"
+                height="1"
+              />
+            </form>
+          </p>
+        </Jumbotron>
+      </div>
+    );
   }
 }
 
